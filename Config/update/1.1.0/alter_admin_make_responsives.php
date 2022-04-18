3265391fd31cf9e0dffbca0cf740a741c3513b98
gussan-beedan <momo.gussan@gmail.com>
2022-04-18 15:53:44 +0900

    構文エラー訂正

--- Config/update/1.1.0/alter_admin_make_responsives.php
+++ Config/update/1.1.0/alter_admin_make_responsives.php
@@ -14,8 +14,8 @@ class AdminMakeResponsivesSchema extends CakeSchema {
 		'id' => ['type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary', 'comment' => 'ID'],
 		'flg_enable' => ['type' => 'boolean', 'null' => true, 'default' => null, 'comment' => 'レスポンシブ有効化'],
 		'admin_color_enable' => ['type' => 'boolean', 'null' => true, 'default' => null, 'comment' => 'テーマカラー有効化'],
-		'admin_color_main' => ['type' => 'text', 'null' => true, 'default' => null 'comment' => 'メインカラー'],
-		'admin_color_sub' => ['type' => 'text', 'null' => true, 'default' => null 'comment' => 'サブカラー'],
+		'admin_color_main' => ['type' => 'text', 'null' => true, 'default' => null, 'comment' => 'メインカラー'],
+		'admin_color_sub' => ['type' => 'text', 'null' => true, 'default' => null, 'comment' => 'サブカラー'],
 		'modified' => ['type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '更新日時'],
 		'created' => ['type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '作成日時'],
 		'indexes' => [
