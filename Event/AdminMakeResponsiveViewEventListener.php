b1b24e73350da18ac0c1cf390c210c05bc410657
gussan-beedan <momo.gussan@gmail.com>
2022-05-01 01:48:43 +0900

    「有効化する」チェックオンオフ時のアドオンファイルの条件を再整理。 fixed-#13

--- Event/AdminMakeResponsiveViewEventListener.php
+++ Event/AdminMakeResponsiveViewEventListener.php
@@ -9,24 +9,24 @@ class AdminMakeResponsiveViewEventListener extends BcViewEventListener {
 		App::import('Model', 'AdminMakeResponsive.AdminMakeResponsive');
 		$_setdatas = new AdminMakeResponsive();
 		$setdatas = $_setdatas->getSetdatas();
-		
-		if (BcUtil::loginUser()) {
-			// ログイン時
-			if (
-				(isset($setdatas))
-				&&(isset($setdatas['flg_enable']))
-				&&($setdatas['flg_enable'] == 0)
-				&&(isset($setdatas['admin_color_enable']))
-				&&($setdatas['admin_color_enable'] == 0)
-			) {
-				// ＋レスポンシブ無効　＋テーマカラー無効
-				return;
-			};
 
+		if (
+			(isset($setdatas))
+			&&(isset($setdatas['flg_enable']))
+			&&($setdatas['flg_enable'] == 0)
+			&&(isset($setdatas['admin_color_enable']))
+			&&($setdatas['admin_color_enable'] == 0)
+		) {
+		// ＋レスポンシブ無効　＋テーマカラー無効
+			return;
+		};
+
+		if (BcUtil::loginUser()) {
+		// ログイン時
 			if ((BcUtil::isAdminSystem())
 				&&(Configure::read('BcSite.admin_theme') === 'admin-third' || $View->viewVars['siteConfig']['admin_theme'] === 'admin-third')
 			) {
-				// admin-third（管理画面）
+			// admin-third（管理画面）
 				if (
 					(isset($setdatas))
 					&&(isset($setdatas['flg_enable']))
@@ -34,7 +34,7 @@ class AdminMakeResponsiveViewEventListener extends BcViewEventListener {
 					&&(isset($setdatas['admin_color_enable']))
 					&&($setdatas['admin_color_enable'] == 1)
 				) {
-					// ＋レスポンシブ有効　＋テーマカラー有効
+				// ＋レスポンシブ有効　＋テーマカラー有効
 					$View->start('admin_make_responsive');
 					$AdminMakeResponsive = $View->element('AdminMakeResponsive.admin/admin_make_responsive_addfiles',['setdatas' => $setdatas]);
 					$AdminMakeResponsive = $View->element('AdminMakeResponsive.admin/admin_make_responsive_addcolorfiles',['setdatas' => $setdatas]);
@@ -45,7 +45,7 @@ class AdminMakeResponsiveViewEventListener extends BcViewEventListener {
 					&&(isset($setdatas['flg_enable']))
 					&&($setdatas['flg_enable'] == 1)
 				) {
-					// ＋レスポンシブ有効　＋テーマカラー無効
+				// ＋レスポンシブ有効　＋テーマカラー無効
 					$View->start('admin_make_responsive');
 					$AdminMakeResponsive = $View->element('AdminMakeResponsive.admin/admin_make_responsive_addfiles',['setdatas' => $setdatas]);
 					$View->end();
@@ -55,19 +55,19 @@ class AdminMakeResponsiveViewEventListener extends BcViewEventListener {
 					&&(isset($setdatas['admin_color_enable']))
 					&&($setdatas['admin_color_enable'] == 1)
 				) {
-					// ＋レスポンシブ無効　＋テーマカラー有効
+				// ＋レスポンシブ無効　＋テーマカラー有効
 					$View->start('admin_make_responsive');
 					$AdminMakeResponsive = $View->element('AdminMakeResponsive.admin/admin_make_responsive_addcolorfiles',['setdatas' => $setdatas]);
 					$View->end();
 					$View->append('css', $AdminMakeResponsive);
 				} else {
-					// ＋レスポンシブ無効　＋テーマカラー無効
+				// ＋レスポンシブ無効　＋テーマカラー無効
 					return;
 				}
 			} else if (
 				(Configure::read('BcSite.admin_theme') === 'admin-third' || $View->viewVars['siteConfig']['admin_theme'] === 'admin-third')
 			) {
-				// admin-third（ユーザー画面上管理ツールバー）
+			// admin-third（ユーザー画面上管理ツールバー）
 				if (
 					(isset($setdatas))
 					&&(isset($setdatas['flg_enable']))
@@ -75,7 +75,7 @@ class AdminMakeResponsiveViewEventListener extends BcViewEventListener {
 					&&(isset($setdatas['admin_color_enable']))
 					&&($setdatas['admin_color_enable'] == 1)
 				) {
-					// ＋レスポンシブ有効　＋テーマカラー有効
+				// ＋レスポンシブ有効　＋テーマカラー有効
 					$View->start('admin_make_responsive');
 					$AdminMakeResponsive = $View->element('AdminMakeResponsive.admin/admin_make_responsive_toolbar_addfiles',['setdatas' => $setdatas]);
 					$AdminMakeResponsive = $View->element('AdminMakeResponsive.admin/admin_make_responsive_toolbar_addcolorfiles',['setdatas' => $setdatas]);
@@ -86,7 +86,7 @@ class AdminMakeResponsiveViewEventListener extends BcViewEventListener {
 					&&(isset($setdatas['flg_enable']))
 					&&($setdatas['flg_enable'] == 1)
 				) {
-					// ＋レスポンシブ有効　＋テーマカラー無効
+				// ＋レスポンシブ有効　＋テーマカラー無効
 					$View->start('admin_make_responsive');
 					$AdminMakeResponsive = $View->element('AdminMakeResponsive.admin/admin_make_responsive_toolbar_addfiles',['setdatas' => $setdatas]);
 					$View->end();
@@ -96,13 +96,13 @@ class AdminMakeResponsiveViewEventListener extends BcViewEventListener {
 					&&(isset($setdatas['admin_color_enable']))
 					&&($setdatas['admin_color_enable'] == 1)
 				) {
-					// ＋レスポンシブ無効　＋テーマカラー有効
+				// ＋レスポンシブ無効　＋テーマカラー有効
 					$View->start('admin_make_responsive');
 					$AdminMakeResponsive = $View->element('AdminMakeResponsive.admin/admin_make_responsive_toolbar_addcolorfiles',['setdatas' => $setdatas]);
 					$View->end();
 					$View->append('css', $AdminMakeResponsive);
 				} else {
-					// ＋レスポンシブ無効　＋テーマカラー無効
+				// ＋レスポンシブ無効　＋テーマカラー無効
 					return;
 				}
 			} else if (
@@ -110,20 +110,20 @@ class AdminMakeResponsiveViewEventListener extends BcViewEventListener {
 				&&(isset($setdatas['flg_enable']))
 				&&($setdatas['flg_enable'] == 1)
 			) {
-				// admin-second（管理画面、ユーザー画面上管理ツールバー 共通）
-				// ＋レスポンシブ有効
+			// admin-second（管理画面、ユーザー画面上管理ツールバー 共通）
+			// ＋レスポンシブ有効
 				$View->start('admin_make_responsive');
 				$AdminMakeResponsive = $View->element('AdminMakeResponsive.admin/admin_go_responsive_addfiles',['setdatas' => $setdatas]);
 				$View->end();
 				$View->append('css', $AdminMakeResponsive);
 			} else {
-				// ＋レスポンシブ無効
+			// ＋レスポンシブ無効
 				return;
 			}
 		} else if (
 			(Configure::read('BcSite.admin_theme') === 'admin-third' || $View->viewVars['siteConfig']['admin_theme'] === 'admin-third')
 		) {
-			// ログオフ時 admin-third（ログイン画面対象）
+		// ログオフ時 admin-third（ログイン画面対象）
 			if (
 				(isset($setdatas))
 				&&(isset($setdatas['flg_enable']))
@@ -131,7 +131,7 @@ class AdminMakeResponsiveViewEventListener extends BcViewEventListener {
 				&&(isset($setdatas['admin_color_enable']))
 				&&($setdatas['admin_color_enable'] == 1)
 			) {
-				// ＋レスポンシブ有効　＋テーマカラー有効
+			// ＋レスポンシブ有効　＋テーマカラー有効
 				$View->start('admin_make_responsive');
 				$AdminMakeResponsive = $View->element('AdminMakeResponsive.admin/admin_make_responsive_toolbar_addfiles',['setdatas' => $setdatas]);
 				$AdminMakeResponsive = $View->element('AdminMakeResponsive.admin/admin_make_responsive_toolbar_addcolorfiles',['setdatas' => $setdatas]);
@@ -142,7 +142,7 @@ class AdminMakeResponsiveViewEventListener extends BcViewEventListener {
 				&&(isset($setdatas['flg_enable']))
 				&&($setdatas['flg_enable'] == 1)
 			) {
-				// ＋レスポンシブ有効　＋テーマカラー無効
+			// ＋レスポンシブ有効　＋テーマカラー無効
 				$View->start('admin_make_responsive');
 				$AdminMakeResponsive = $View->element('AdminMakeResponsive.admin/admin_make_responsive_toolbar_addfiles',['setdatas' => $setdatas]);
 				$View->end();
@@ -152,21 +152,29 @@ class AdminMakeResponsiveViewEventListener extends BcViewEventListener {
 				&&(isset($setdatas['admin_color_enable']))
 				&&($setdatas['admin_color_enable'] == 1)
 			) {
-				// ＋レスポンシブ無効　＋テーマカラー有効
+			// ＋レスポンシブ無効　＋テーマカラー有効
 				$View->start('admin_make_responsive');
 				$AdminMakeResponsive = $View->element('AdminMakeResponsive.admin/admin_make_responsive_toolbar_addcolorfiles',['setdatas' => $setdatas]);
 				$View->end();
 				$View->append('css', $AdminMakeResponsive);
 			} else {
-				// （レスポンシブ無効）（テーマカラー無効）
+			// （レスポンシブ無効）（テーマカラー無効）
 				return;
 			}
-		} else {
-			// ログオフ時 admin-second（ログイン画面対象）
+		} else if (
+			(isset($setdatas))
+			&&(isset($setdatas['flg_enable']))
+			&&($setdatas['flg_enable'] == 1)
+		) {
+		// ログオフ時 admin-second（ログイン画面対象）
+		// ＋レスポンシブ有効
 			$View->start('admin_make_responsive');
 			$AdminMakeResponsive = $View->element('AdminMakeResponsive.admin/admin_go_responsive_toolbar_addfiles',['setdatas' => $setdatas]);
 			$View->end();
 			$View->append('css', $AdminMakeResponsive);
+		} else {
+		// ＋レスポンシブ無効
+			return;
 		}
 	}
 }
